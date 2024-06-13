0041554d5792e04859f30325d3f3bc96ebdbba8c
BearHumanS <bye0651@naver.com>
2024-06-13 20:56:15 +0900

    chore: 임시로 사용하는 데이터

---
+++ src/lib/constants/mocking.ts
@@ -0,0 +1,19 @@
+export const MAIN_MOCKING = {
+  id: 0,
+  username: "아이어를위하여",
+  img: "/next.svg",
+  role: "general",
+  createdAt: "2024-06-12T05:34:39.285Z",
+  fandom: "유애나",
+  news: [
+    {
+      1: "아이유 vs 젤나가 누가 제일 잘나가나?",
+      2: "아이유, '제라툴 맞짱뜨자' 발언!",
+      3: "속보! 아이유, 제라툴이랑 맞짱!",
+    },
+  ],
+  recently: [
+    { latter: "/vercel.svg", title: "칼날여왕 ㅗdfㅗ" },
+    { latter: "/vercel.svg", title: "칼날여왕 ㅗㅗ" },
+  ],
+};
