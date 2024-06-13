02c22a3bac85c55022695b2603c2115ca06f957b
BearHumanS <bye0651@naver.com>
2024-06-13 21:01:25 +0900

    fix: css파일 이름 변경에 따른 import path 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import NavBar from "@/components/common/navbar/NavBar";
-import "./global.css";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
