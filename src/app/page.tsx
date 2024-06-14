1741a4bbfcb12f55e19877a7811a269005e2e68a
BearHumanS <bye0651@naver.com>
2024-06-15 00:33:37 +0900

    feat: 메인 페이지 작업 내용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,30 @@
+"use client";
+
+import Image from "next/image";
 import ArticleSection from "@/components/main/ArticleSection";
 import DateSection from "@/components/main/DateSection";
 import RecentLetterSection from "@/components/main/RecentLetterSection";
 import { MAIN_MOCKING } from "@/lib/constants/mocking";
 
 export default function Home() {
+  const test = () => {
+    console.log("편지 쓰기");
+  };
   return (
     <main>
-      <h1>
-        {MAIN_MOCKING.username} 님, {MAIN_MOCKING.fandom}에 오신걸 환영합니다!
-      </h1>
+      <h1>{MAIN_MOCKING.username} 님, 환영합니다!</h1>
 
       <ArticleSection />
 
       <DateSection />
 
       <RecentLetterSection />
+
+      <div className="flex flex-row-reverse w-[343px] fixed z-10 bottom-24 pointer-events-none">
+        <button onClick={test} className="pointer-events-auto">
+          <Image src="/편지쓰기.png" width={48} height={48} alt="편지쓰기" />
+        </button>
+      </div>
     </main>
   );
 }
