1ecb26e462cb781117037839f90360e3e2a7b192
BearHumanS <bye0651@naver.com>
2024-06-15 00:32:40 +0900

    feat: 메인 페이지 최근 편지 부분 작업

--- src/components/main/DynamicLetterComponet.tsx
+++ src/components/main/DynamicLetterComponet.tsx
@@ -4,10 +4,13 @@ import Image from "next/image";
 import { MAIN_MOCKING } from "@/lib/constants/mocking";
 
 function DynamicLetterComponet() {
+  const test = () => {
+    console.log(1);
+  };
   return (
-    <div className="flex justify-center gap-2">
+    <div className="gap-2 grid grid-cols-2">
       {MAIN_MOCKING.recently.map((item, index) => (
-        <div key={`${item.title}-${index}`} className="flex flex-col gap-2">
+        <div onClick={test} key={`${item.title}-${index}`} className="flex flex-col gap-2 cursor-pointer">
           <div className="border-2 rounded-lg w-[168px] h-[90px]">
             <Image src={item.latter} width={168} height={90} alt={`편지 ${item.title}`} />
           </div>
