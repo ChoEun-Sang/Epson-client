e8776089d4ab8f3ad1fa57704c803c8ba92f91c1
ChaDongmin <cdm1263@gmail.com>
2024-06-16 00:17:38 +0900

    feat: 메일 상세 페이지 제작

---
+++ src/app/maildetail/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import MailDescription from "@/components/mailDetail/MailDescription";
+import Mailcontents from "@/components/mailDetail/Mailcontents";
+import { MAIL_MOCKING } from "@/lib/constants/mocking";
+
+function MailDeatil() {
+  const { title, letterImageUrl, createdAt, sender, originalText, translatedText } = MAIL_MOCKING;
+  const mailDescriptionData = { title, letterImageUrl, createdAt, sender };
+  const mailContentsData = { originalText, translatedText };
+  return (
+    <section className="flex flex-col gap-y-6">
+      <MailDescription {...mailDescriptionData} />
+      <Mailcontents {...mailContentsData} />
+    </section>
+  );
+}
+
+export default MailDeatil;
