9502435000d66cfc1ba6f7f5e8652bc5a60c151a
ChaDongmin <cdm1263@gmail.com>
2024-06-17 19:44:39 +0900

    feat: 키워드를 제거하는 deleteKeyword 함수 추가

--- src/store/useMailDetailStore.ts
+++ src/store/useMailDetailStore.ts
@@ -7,6 +7,7 @@ interface useMailDetailStore {
   setSelectedText: (text: string) => void;
   setSelectedTranslatedText: (text: string) => void;
   addKeyword: (keyword: string) => void;
+  deleteKeyword: (keyword: string) => void;
 }
 
 const useMailDetailStore = create<useMailDetailStore>((set) => ({
@@ -16,6 +17,7 @@ const useMailDetailStore = create<useMailDetailStore>((set) => ({
   setSelectedText: (text) => set({ selectedText: text }),
   setSelectedTranslatedText: (text) => set({ selectedTranslatedText: text }),
   addKeyword: (keyword) => set((state) => ({ keywords: [...state.keywords, keyword] })),
+  deleteKeyword: (keyword) => set((state) => ({ keywords: state.keywords.filter((word) => word !== keyword) })),
 }));
 
 export default useMailDetailStore;
