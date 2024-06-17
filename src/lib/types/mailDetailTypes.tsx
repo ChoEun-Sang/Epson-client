faeef2dd5f604896f8b429cb504bb06b259ea2f4
ChaDongmin <cdm1263@gmail.com>
2024-06-17 19:44:16 +0900

    feat: LetterDetailInfo, LetterDetailDocument 타입 추가

--- src/lib/types/mailDetailTypes.tsx
+++ src/lib/types/mailDetailTypes.tsx
@@ -9,3 +9,39 @@ export interface MailText {
   originalText: string[];
   translatedText: string[];
 }
+
+export interface LetterDetailInfo {
+  letter: {
+    id: string;
+    title: "string";
+    letterDocumentId: "string";
+    createdAt: "string";
+    sender: {
+      id: 0;
+      username: "string";
+      img: unknown;
+      role: "string";
+      createdAt: "string";
+    };
+    receiver: {
+      id: 0;
+      username: "string";
+      img: unknown;
+      role: "string";
+      createdAt: "string";
+    };
+  };
+}
+
+export interface LetterDetailDocument {
+  _id: "string";
+  pages: [
+    {
+      type: "string";
+      url: "string";
+      originText: string[];
+      translatedText: string[];
+    }
+  ];
+  letterId: 0;
+}
