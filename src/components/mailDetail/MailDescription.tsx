97d8e82da0d64386783f1da57081d6ef72a45482
ChaDongmin <cdm1263@gmail.com>
2024-06-18 02:56:52 +0900

    feat: 원본 편시 보기 버튼 추가

--- src/components/mailDetail/MailDescription.tsx
+++ src/components/mailDetail/MailDescription.tsx
@@ -2,17 +2,20 @@
 
 import { MailDetailData } from "@/lib/types/mailDetailTypes";
 import { formatDate } from "@/lib/util/utilFunctions";
+import { Button } from "../ui/button";
 
 function MailDescription({ title, letterImageUrl, createdAt, sender }: MailDetailData) {
   return (
-    <div className="w-full flex flex-col">
+    <div className="w-full flex flex-col gap-y-1">
       <p className="font-bold text-[22px] text-text-sub">{title}</p>
-      <div className="flex justify-between">
-        <div className="flex flex-col">
+      <div className="flex justify-between items-end">
+        <div className="flex flex-col gap-y-1">
           <p className="font-medium text-text-info">from.{sender}</p>
           <p className="text-xs text-text-disabled">{formatDate(createdAt)}</p>
         </div>
-        <button>원본 보기</button>
+        <Button variant="destructive" className="bg-primary-8 font-bold text-xs px-2 h-[32px]" onClick={() => {}}>
+          원본보기
+        </Button>
       </div>
     </div>
   );
