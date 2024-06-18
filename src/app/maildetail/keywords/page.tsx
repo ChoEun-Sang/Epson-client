6eb4b2738bc92ee6ad01a28920ee76b2cc98e4fb
ChaDongmin <cdm1263@gmail.com>
2024-06-19 03:10:22 +0900

    feat: useGetGPTDataQueries 적용 및 임시 되돌아가기 버튼 생성

--- src/app/maildetail/keywords/page.tsx
+++ src/app/maildetail/keywords/page.tsx
@@ -3,6 +3,7 @@
 import AddedKeywordsList from "@/components/mailDetail/AddedKeywordsList";
 import KeywordDetails from "@/components/mailDetail/KeywordDetails";
 import KeywordSlider from "@/components/mailDetail/KeywordSlider";
+import useGetGPTDataQueries from "@/hooks/queries/useGetGPTDataQuery";
 import { getKeywordsInSentence } from "@/lib/util/utilFunctions";
 import useMailDetailStore from "@/store/useMailDetailStore";
 import { useRouter } from "next/navigation";
@@ -15,6 +16,7 @@ function SelectKeywords() {
   const keywordsInSentence = getKeywordsInSentence(selectedText);
   const currentKeyword = keywordsInSentence[currentKeywordIndex];
   const isKeywordIncluded = keywords.includes(currentKeyword);
+  const queries = useGetGPTDataQueries(keywordsInSentence);
 
   // 추후 letterId로 동적 라우팅 처리
   useEffect(() => {
@@ -25,8 +27,15 @@ function SelectKeywords() {
 
   return (
     <section className="flex flex-col gap-y-3 h-full">
+      <button className="bg-yellow-200" onClick={() => router.replace("/maildetail/")}>
+        임시 되돌아가기 버튼
+      </button>
       <KeywordSlider currentKeywordIndex={currentKeywordIndex} setCurrentKeywordIndex={setCurrentKeywordIndex} />
-      <KeywordDetails currentKeyword={currentKeyword} isKeywordIncluded={isKeywordIncluded} />
+      <KeywordDetails
+        currentKeyword={currentKeyword}
+        isKeywordIncluded={isKeywordIncluded}
+        gptData={queries[currentKeywordIndex]}
+      />
       <AddedKeywordsList />
     </section>
   );
