e9ec43481bab5bad353a21a03ce31e64fee4cad2
ChaDongmin <cdm1263@gmail.com>
2024-06-19 03:11:04 +0900

    feat: 각 키워드에 대한 gpt 데이터 적용

--- src/components/mailDetail/KeywordDetails.tsx
+++ src/components/mailDetail/KeywordDetails.tsx
@@ -1,17 +1,25 @@
 "use client";
 
 import useMailDetailStore from "@/store/useMailDetailStore";
+import { UseQueryResult } from "@tanstack/react-query";
+import Image from "next/image";
 
 interface KeywordDetailsProps {
   currentKeyword: string;
   isKeywordIncluded: boolean;
+  gptData: UseQueryResult<string>;
 }
 
-function KeywordDetails({ currentKeyword, isKeywordIncluded }: KeywordDetailsProps) {
+function KeywordDetails({ currentKeyword, isKeywordIncluded, gptData }: KeywordDetailsProps) {
   const { addKeyword, deleteKeyword } = useMailDetailStore();
+  const { data, isLoading, error } = gptData;
+
+  // 임시 로딩
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
 
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col gap-y-2 mb-6">
       <div className="flex gap-x-2">
         <span className="font-bold text-primary-5 text-xs">뜻</span>
         <div className="flex flex-col w-full">
@@ -26,10 +34,32 @@ function KeywordDetails({ currentKeyword, isKeywordIncluded }: KeywordDetailsPro
               {isKeywordIncluded ? "제거하기" : "추가하기"}
             </button>
           </div>
-          <span className="text-sm font-semibold text-text-disabled">[영문 뜻]</span>
+          <span className="text-sm font-semibold text-text-disabled">[{data && JSON.parse(data).translated}]</span>
         </div>
       </div>
-      <div className="bg-yellow-100">번역 api 내용 들어가는 곳</div>
+      <div className="flex flex-col gap-y-2">
+        <p className="font-medium text-text-info">{data && JSON.parse(data).definition}</p>
+        <p className="font-medium text-text-info border-l-2 border-gray-3 pl-2">{data && JSON.parse(data).enexample}</p>
+        <p className="font-medium text-text-info border-l-2 border-gray-3 pl-2">{data && JSON.parse(data).krexample}</p>
+        <ul className="flex gap-x-1.5 border-l-2 border-gray-3 flex-wrap pl-2">
+          <Image src="/equal.svg" alt="" width={24} height={24} />
+          {data &&
+            JSON.parse(data).synonyms.map((synonym: string, index: number) => (
+              <span className="font-medium text-text-info" key={index}>
+                {synonym}
+              </span>
+            ))}
+        </ul>
+        <ul className="flex gap-x-1.5 border-l-2 border-gray-3 flex-wrap pl-2">
+          <Image src="/width.svg" alt="" width={24} height={24} />
+          {data &&
+            JSON.parse(data).antonyms.map((antonym: string, index: number) => (
+              <span className="font-medium text-text-info" key={index}>
+                {antonym}
+              </span>
+            ))}
+        </ul>
+      </div>
     </div>
   );
 }
