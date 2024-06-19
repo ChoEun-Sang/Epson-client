09d3d1390687c07489d6b4e4f9916209e2800da4
BearHumanS <bye0651@naver.com>
2024-06-19 15:06:28 +0900

    feat: 인쇄 관련 헤더

---
+++ src/components/header/PrintHeader.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import BackButton from "../common/button/BackButton";
+import { usePathname } from "next/navigation";
+import { checkPathname } from "./RenderHeader";
+
+function PrintHeader() {
+  const pathname = usePathname();
+
+  const { materialDetail } = checkPathname(pathname);
+
+  return (
+    <div className="flex items-end w-full">
+      <BackButton />
+
+      <div className="flex justify-end w-full gap-6 py-2">
+        {materialDetail ? (
+          ""
+        ) : (
+          <button>
+            <Image src="/custom_typography.png" width={18} height={18} alt="custom_typography" />
+          </button>
+        )}
+        <button>
+          <Image src="/print.png" width={24} height={24} alt="print" />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default PrintHeader;
