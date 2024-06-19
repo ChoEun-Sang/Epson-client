65518cf7ae06bd2b0b2f8164e0143a7f9ba5bd63
ChaDongmin <cdm1263@gmail.com>
2024-06-19 21:51:14 +0900

    design: sonner 컴포넌트 스타일 변경

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,31 +1,32 @@
-"use client"
+"use client";
 
-import { useTheme } from "next-themes"
-import { Toaster as Sonner } from "sonner"
+import { useTheme } from "next-themes";
+import { Toaster as Sonner } from "sonner";
 
-type ToasterProps = React.ComponentProps<typeof Sonner>
+type ToasterProps = React.ComponentProps<typeof Sonner>;
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme()
-
+  const { theme = "system" } = useTheme();
   return (
     <Sonner
       theme={theme as ToasterProps["theme"]}
       className="toaster group"
+      position="bottom-center"
       toastOptions={{
+        unstyled: true,
         classNames: {
           toast:
-            "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
-          description: "group-[.toast]:text-muted-foreground",
-          actionButton:
-            "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
-          cancelButton:
-            "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
+            "group toast group-[.toaster]:bg-black group-[.toaster]:text-white group-[.toaster]:w-[343px] group-[.toaster]:rounded-lg group-[.toaster]:h-[48px] flex items-center px-4",
+          title: "group-[.toast]:text-xs group-[.toast]:text-white ml-2",
+          // description: "group-[.toast]:text-sm group-[.toast]:text-white",
+          actionButton: "group-[.toast]:text-xs group-[.toast]:text-primary-5 group-[.toast]:font-bold",
+          // cancelButton: "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
+          icon: "group-data-[type=error]:text-red-500 group-data-[type=success]:text-green-500 group-data-[type=warning]:text-amber-500 group-data-[type=info]:text-blue-500",
         },
       }}
       {...props}
     />
-  )
-}
+  );
+};
 
-export { Toaster }
+export { Toaster };
