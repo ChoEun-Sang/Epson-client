73d1d374d6b68980a8c869bb2c1fdd9c94272572
BearHumanS <bye0651@naver.com>
2024-06-19 15:06:15 +0900

    feat: 페이지 별 헤더 변경

--- src/components/header/RenderHeader.tsx
+++ src/components/header/RenderHeader.tsx
@@ -1,26 +1,41 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import MainHeader from "./MainHeader";
-import SendHeader from "./SendHeader";
+import UserHeader from "./UserHeader";
+import SimpleHeader from "./SimpleHeader";
+import BackHeader from "./BackHeader";
+import { KEYWORDS_PATH, MATERIAL_DETAIL_PATH, ORIGINALIMAGE_PATH, PRINT_PATH } from "@/lib/constants/pathname";
+import PrintHeader from "./PrintHeader";
+
+export const checkPathname = (pathname: string) => {
+  const originalImage = ORIGINALIMAGE_PATH.test(pathname);
+  const print = PRINT_PATH.test(pathname);
+  const keywords = KEYWORDS_PATH.test(pathname);
+  const materialDetail = MATERIAL_DETAIL_PATH.test(pathname);
+
+  return { originalImage, print, keywords, materialDetail };
+};
 
 function RenderHeader() {
   const pathname = usePathname();
 
-  const renderHeader = () => {
-    if (pathname === "/") {
-      return <MainHeader />;
-    }
+  const { originalImage, print, keywords, materialDetail } = checkPathname(pathname);
 
-    if (pathname === "/send") {
-      return <SendHeader />;
-    }
-    if (pathname === "/mail") {
-      return <div>mail</div>;
-    }
-  };
+  const sendmailPaths = ["/sendmail", "/sendmail/scan"];
+  const userHeaderGroup = ["/", "/mailbox", "/material"];
 
-  return renderHeader();
+  const isSendmailPath = sendmailPaths.includes(pathname);
+  const isUserHeader = userHeaderGroup.includes(pathname);
+
+  if (isUserHeader) {
+    return <UserHeader />;
+  } else if (isSendmailPath || originalImage || print) {
+    return <SimpleHeader />;
+  } else if (keywords || materialDetail) {
+    return <PrintHeader />;
+  } else {
+    return <BackHeader />;
+  }
 }
 
 export default RenderHeader;
