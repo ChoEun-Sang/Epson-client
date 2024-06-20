2b441cee7d7edcd4694ce2dd2c19b3423e12a7b4
BearHumanS <bye0651@naver.com>
2024-06-21 00:18:51 +0900

    feat: 일부 페이지 네비게이션 렌더링 되지않게 추가

--- src/components/common/navbar/NavBar.tsx
+++ src/components/common/navbar/NavBar.tsx
@@ -1,17 +1,45 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import NavMenu from "./NavMenu";
 import { ITEMS } from "@/lib/constants/navMenuItems";
+import {
+  MAILSCAN_PATH,
+  MAILUPLOAD_PATH,
+  SCAN_PATH,
+  KEYWORDS_PATH,
+  PRINT_PATH,
+  ORIGINALIMAGE_PATH,
+  MATERIAL_DETAIL_PATH,
+} from "@/lib/constants/pathname";
 
 const NavBar = () => {
+  const pathname = usePathname();
+
+  const invisiblePaths = [
+    MAILSCAN_PATH,
+    MAILUPLOAD_PATH,
+    SCAN_PATH,
+    KEYWORDS_PATH,
+    PRINT_PATH,
+    ORIGINALIMAGE_PATH,
+    MATERIAL_DETAIL_PATH,
+  ];
+
+  const invisibleNav = invisiblePaths.includes(pathname);
+
   return (
-    <nav className="bg-background max-w-[375px] h-20 sticky bottom-0 left-0 right-0 mx-auto">
-      <ul className="h-full flex justify-around items-center">
-        {ITEMS.map((item) => (
-          <NavMenu key={item.ko} path={item.path}>
-            {item.ko}
-          </NavMenu>
-        ))}
-      </ul>
-    </nav>
+    !invisibleNav && (
+      <nav className="bg-background max-w-[375px] h-20 sticky bottom-0 left-0 right-0 mx-auto">
+        <ul className="h-full flex justify-around items-center">
+          {ITEMS.map((item) => (
+            <NavMenu key={item.ko} path={item.path}>
+              {item.ko}
+            </NavMenu>
+          ))}
+        </ul>
+      </nav>
+    )
   );
 };
 
