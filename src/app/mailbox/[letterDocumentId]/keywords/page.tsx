0607f552d86c4505a68c0a48847a2b9401dea966
ChaDongmin <cdm1263@gmail.com>
2024-06-20 16:42:27 +0900

    feat: 편지 상세보기 페이지를 mailbox 페이지와 연결

---
+++ src/app/mailbox/[letterDocumentId]/keywords/page.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import AddedKeywordsList from "@/components/mailDetail/AddedKeywordsList";
+import KeywordDetails from "@/components/mailDetail/KeywordDetails";
+import KeywordSlider from "@/components/mailDetail/KeywordSlider";
+import useGetGPTDataQueries from "@/hooks/queries/useGetGPTDataQuery";
+import { getKeywordsInSentence } from "@/lib/util/utilFunctions";
+import useMailDetailStore from "@/store/useMailDetailStore";
+import { useParams, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+function SelectKeywords() {
+  const router = useRouter();
+  const [currentKeywordIndex, setCurrentKeywordIndex] = useState<number>(0);
+  const { selectedText, keywords } = useMailDetailStore();
+  const keywordsInSentence = getKeywordsInSentence(selectedText);
+  const currentKeyword = keywordsInSentence[currentKeywordIndex];
+  const isKeywordIncluded = keywords.includes(currentKeyword);
+  const queries = useGetGPTDataQueries(keywordsInSentence);
+  const { letterDocumentId } = useParams();
+
+  useEffect(() => {
+    if (!selectedText) {
+      router.replace(`/mailbox/${letterDocumentId}`);
+    }
+  }, [selectedText, router, letterDocumentId]);
+
+  return (
+    <section className="flex flex-col gap-y-3 h-full">
+      <KeywordSlider currentKeywordIndex={currentKeywordIndex} setCurrentKeywordIndex={setCurrentKeywordIndex} />
+      <KeywordDetails
+        currentKeyword={currentKeyword}
+        isKeywordIncluded={isKeywordIncluded}
+        gptData={queries[currentKeywordIndex]}
+      />
+      <AddedKeywordsList />
+    </section>
+  );
+}
+
+export default SelectKeywords;
