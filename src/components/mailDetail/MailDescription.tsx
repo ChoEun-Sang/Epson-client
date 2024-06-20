031c720f2ed3f9b2f6c63608b871c41f65a3ef10
ChaDongmin <cdm1263@gmail.com>
2024-06-20 20:25:04 +0900

    refactor: CustomDialog에서 트리거 버튼을 컴포넌트 바깥으로 이동

--- src/components/mailDetail/MailDescription.tsx
+++ src/components/mailDetail/MailDescription.tsx
@@ -3,6 +3,8 @@
 import { LetterDetailInfo } from "@/lib/types/mailDetailTypes";
 import { formatDate } from "@/lib/util/utilFunctions";
 import CustomDialog from "../common/CustomDialog";
+import { Dialog, DialogTrigger } from "../ui/dialog";
+import { Button } from "../ui/button";
 
 interface MailDescriptionProps {
   letterImageUrl: string | undefined;
@@ -18,15 +20,22 @@ function MailDescription({ letterImageUrl, letterInfoData }: MailDescriptionProp
           <p className="font-medium text-text-info">from.{letterInfoData?.sender.username}</p>
           <p className="text-xs text-text-disabled">{letterInfoData && formatDate(letterInfoData?.createdAt)}</p>
         </div>
-        <CustomDialog title="편지 원본">
-          <iframe
-            scrolling="no"
-            className="main"
-            src={`https://aigooback.blob.core.windows.net${letterImageUrl}#toolbar=0&navpanes=0&scrollbar=0`}
-            width={"100%"}
-            height={"100%"}
-          />
-        </CustomDialog>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="destructive" className="bg-primary-8 font-bold text-xs px-2 h-[32px]">
+              원본보기
+            </Button>
+          </DialogTrigger>
+          <CustomDialog title="편지 원본">
+            <iframe
+              scrolling="no"
+              className="main"
+              src={`https://aigooback.blob.core.windows.net${letterImageUrl}#toolbar=0&navpanes=0&scrollbar=0`}
+              width={"100%"}
+              height={"100%"}
+            />
+          </CustomDialog>
+        </Dialog>
       </div>
     </div>
   );
