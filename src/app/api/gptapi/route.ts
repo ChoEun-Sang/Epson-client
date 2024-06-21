256d26949657f60dd7f4f5b110f3ef3effdf78cd
ChaDongmin <cdm1263@gmail.com>
2024-06-21 22:35:22 +0900

    refactor: GPT 프롬프트 명시적으로 업데이트

--- src/app/api/gptapi/route.ts
+++ src/app/api/gptapi/route.ts
@@ -19,7 +19,7 @@ export const POST = async (req: NextRequest) => {
         {
           role: "system",
           content:
-            "keyword에 적힌 글을 분석해서 모두 json 객체에 담아서 알려줘. keyword가 한글이라면 translated에는 keyword를 영어로 번역하고 keyword가 영어라면 translated에는 keyword를 한글로 번역해. definition에는 keyword가 한글이라면 영어로 된 사전적 의미를, keyword가 영어라면 한글로 된 사전적 의미를 넣어. synonyms에는 keyword가 한글이라면 한글로 된 동의어를, keyword가 영어라면 영어로 된 동의어를 넣어. antonym에는 keyword가 영어라면 영어로 된 반의어를, keyword가 한글이라면 한글로 된 반의어를 넣어. 추가적으로 keyword가 한글이라면 example에는 한글로 된 한 줄의 예시문장과 translatedExample에는 example에 썼던 예시 문장을 영어로 번역해서 넣고, keyword가 영어라면 example에는 영어로 된 한 줄의 예시문장과 translatedExample에는 example에 썼던 예시 문장을 한글로 번역해서 넣어. 그리고 동사, 형용사, 부사등은 기본형으로 알려줘. 다만 영어가 아닌 것들은 필요없으니 쓰지마.",
+            "keyword를 분석하여 JSON 객체로 반환해. keyword가 한글이라면 translated: keyword를 영어로 번역한 값, definition: keyword의 영어 사전적 의미, synonyms: 한글 동의어 리스트, antonyms: 한글 반의어 리스트, example: 한글로 된 예시 문장, translatedExample: example을 영어로 번역한 값. keyword가 영어라면 translated: keyword를 한글로 번역한 값,definition: keyword의 한글 사전적 의미, synonyms: 영어 동의어 리스트, antonyms: 영어 반의어 리스트, example: 영어로 된 예시 문장, translatedExample: example을 한글로 번역한 값.",
         },
         {
           role: "user",
