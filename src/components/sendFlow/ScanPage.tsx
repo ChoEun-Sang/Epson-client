357844da205c48213e968d46954b8d2a26cbb32c
BearHumanS <bye0651@naver.com>
2024-06-22 06:38:37 +0900

    feat: 편지 보내기 스캔 요청 이후 페이지 구현

---
+++ src/components/sendFlow/ScanPage.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import useScanStore from "@/store/useScanStore";
+import SendLoading from "@/components/sendFlow/SendLoading";
+import ScanGuideLine from "@/components/sendFlow/ScanGuideLine";
+
+function ScanPage() {
+  const { scanned, setScanned } = useScanStore();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (scanned) {
+      const timer = setTimeout(() => {
+        router.push("/");
+      }, 3000);
+      return () => clearTimeout(timer);
+    }
+  }, [scanned, router, setScanned]);
+
+  return scanned ? <SendLoading /> : <ScanGuideLine onScanClick={() => setScanned(true)} />;
+}
+
+export default ScanPage;
