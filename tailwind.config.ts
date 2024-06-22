af33313ac4bcdf9a48ecd25bb444a6eeb0cd1fc6
BearHumanS <bye0651@naver.com>
2024-06-23 07:23:46 +0900

    chore: 색상 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -102,6 +102,7 @@ const config = {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
         },
+        "black-30": "rgba(0, 0, 0, 0.3)",
       },
       borderRadius: {
         lg: "var(--radius)",
