94758ed701541d957f6a0ab46e1b8fd8457452f5
ChaDongmin <cdm1263@gmail.com>
2024-06-24 04:12:15 +0900

    feat: 개발 서버 이미지 path 추가

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "aigooback.blob.core.windows.net",
       },
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
     ],
   },
 };
