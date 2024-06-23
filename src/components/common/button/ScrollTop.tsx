0ef95ab2d0bdbd3efc465b4acd3e7cf7145b7398
ChaDongmin <cdm1263@gmail.com>
2024-06-23 21:20:45 +0900

    design: top 버튼 위치 조정

--- src/components/common/button/ScrollTop.tsx
+++ src/components/common/button/ScrollTop.tsx
@@ -3,11 +3,11 @@ import React from "react";
 
 function ScrollTopBtn({ onClick }: { onClick: () => void }) {
   return (
-    <div className="w-full flex flex-row-reverse pointer-events-none sticky bottom-1">
+    <div className="w-full flex flex-row-reverse pointer-events-none sticky bottom-4">
       <button
         type="button"
         onClick={onClick}
-        className="sticky  right-0 h-12 w-12 flex justify-center items-center rounded-full bg-primary-8 pointer-events-auto"
+        className="sticky right-0 h-12 w-12 flex justify-center items-center rounded-full bg-primary-8 pointer-events-auto"
       >
         <Image src="/vertical_align_top.svg" alt="scroll top button" width={24} height={24} />
       </button>
