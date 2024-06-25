264fac5113fa180c10db0bb2eaf1faa2486d4fdf
BearHumanS <bye0651@naver.com>
2024-06-25 19:27:35 +0900

    feat: 리다이렉트 페이지

---
+++ src/components/RedirectPage.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import Image from "next/image";
+
+function RedirectPage() {
+  return (
+    <section className="innerheight flex justify-center items-center flex-col">
+      <div className="w-full h-full flex flex-col justify-center items-center">
+        <div className="w-60 h-60 rounded-full border-4 overflow-hidden">
+          <Image src="/login.gif" width={240} height={240} alt="loading" />
+        </div>
+        <div className="text-center title2 mt-8">
+          <p className="body3 text-text-info">Log-in is required to use this service!</p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default RedirectPage;
