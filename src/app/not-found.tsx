c4b43e61b726132d6d6d68643cb73a5bec1e182b
BearHumanS <bye0651@naver.com>
2024-06-28 11:04:11 +0900

    refactor: 아이고 로고 이미지 추가

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,6 +3,7 @@
 import useIsCheckedLoadingStroe from "@/store/useIsCheckedLoadingStroe";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import Image from "next/image";
 
 function Redirect() {
   const router = useRouter();
@@ -28,7 +29,8 @@ function Redirect() {
 
   return (
     <section className="innerheight w-full flex justify-center items-center">
-      <div className="text-center">
+      <div className="flex flex-col justify-center items-center gap-2">
+        <Image src="/AIGOO.png" width={177} height={90} alt="Loading_logo" />
         <span className="title1">Invalid page access.</span>
         <div className="mt-2 body2 text-text-info">
           You will be redirected in <span className="text-primary-8">{seconds}</span> seconds.
--- src/components/RedirectPage.tsx
+++ src/components/RedirectPage.tsx
@@ -6,9 +6,9 @@ function RedirectPage() {
   return (
     <section className="innerheight flex justify-center items-center flex-col">
       <div className="w-full h-full flex flex-col justify-center items-center">
-        <div className="w-60 h-60 rounded-full border-4 overflow-hidden">
-          <Image src="/login.gif" width={240} height={240} alt="loading" />
-        </div>
+        {/* <div className="w-60 h-60 rounded-full border-4 overflow-hidden"> */}
+        <Image src="/AIGOO.png" width={177} height={90} alt="loading" />
+        {/*  </div> */}
         <div className="text-center title2 mt-8">
           <p className="body3 text-text-info">Log-in is required to use this service!</p>
         </div>
