8ed7eab3d0c07a29b961cb825aa12e7f127686fa
BearHumanS <bye0651@naver.com>
2024-06-28 11:05:17 +0900

    refactor: 전체 우편함으로 가는 로직 추가

--- src/components/main/RecentLetterSection.tsx
+++ src/components/main/RecentLetterSection.tsx
@@ -1,14 +1,31 @@
+import useUserStore from "@/store/useUserStore";
 import dynamic from "next/dynamic";
+import { useRouter } from "next/navigation";
 
 const DynamicLetterComponet = dynamic(() => import("./DynamicLetterComponet"), {
   ssr: false,
 });
 
 function RecentLetterSection() {
+  const router = useRouter();
+  const { userData } = useUserStore();
+
+  const handleClickMailBox = () => {
+    router.push("/mailbox");
+  };
+
   return (
     <section>
-      <div className="h-[73px] pt-8 pb-2">
+      <div className="h-[73px] pt-8 pb-2 flex justify-between">
         <h2 className="title2">Received Letters</h2>
+        {userData && (
+          <div
+            className="callout2 text-text-info flex items-end cursor-pointer hover:border-b-2 border-b-text-info"
+            onClick={handleClickMailBox}
+          >
+            View All
+          </div>
+        )}
       </div>
 
       <DynamicLetterComponet />
